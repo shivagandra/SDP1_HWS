cd32b89de0e0d3f3036bf0b5ced7783c168f6219
shivagandra <77003885+shivagandra@users.noreply.github.com>
2021-11-10 10:48:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/Pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Projects';
+import SignUp from './components/Pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
